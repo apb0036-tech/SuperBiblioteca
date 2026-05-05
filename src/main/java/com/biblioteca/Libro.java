6304c3d2464c40b75580fdd112cdbfae0c98c92b
Ariel <apb0036@alu.medac.es>
2026-05-05 22:07:29 +0200

    Creacion de la estructura base de la biblioteca

---
+++ src/main/java/com/biblioteca/Libro.java
@@ -0,0 +1,13 @@
+
+
+public class libro {
+
+    private String titulo;
+    private Boolean prestado;
+
+    public libro (String titulo){
+        this.titulo = titulo;
+        this.prestado = false;
+    }
+
+}
